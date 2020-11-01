b9f1640af78a1f3b1eaf11bc7fe647ac3186eee2
rbenjos <rbenjos@gmail.com>
2020-11-01 20:52:02 +0200

    hey

--- drop.sql
+++ drop.sql
@@ -1,1 +1,5 @@
-drop table olympics;
+drop table Olympics;
+drop table Athlete;
+drop table Team;
+drop table Event;
+drop table Competed at;
--- ex1.py
+++ ex1.py
@@ -6,8 +6,17 @@ MAX_LINES = 10000
 
 # opens file for olympics table.
 # CHANGE!
-outfile = open("olympics.csv", 'w',)
-outwriter = csv.writer(outfile, delimiter=",", quoting=csv.QUOTE_NONE)
+Olympics = open("Olympics.csv", 'w',)
+Athlete = open("Athlete.csv", 'w',)
+Team = open("Team.csv", 'w',)
+Event = open("Event.csv", 'w',)
+Competed_at = open("Competed at.csv", 'w',)
+
+outwriter_o = csv.writer(Olympics, delimiter=",", quoting=csv.QUOTE_NONE)
+outwriter_a = csv.writer(Athlete, delimiter=",", quoting=csv.QUOTE_NONE)
+outwriter_t = csv.writer(Team, delimiter=",", quoting=csv.QUOTE_NONE)
+outwriter_e = csv.writer(Event, delimiter=",", quoting=csv.QUOTE_NONE)
+outwriter_c = csv.writer(Competed_at, delimiter=",", quoting=csv.QUOTE_NONE)
 
 # process_file goes over all rows in original csv file, and sends each row to process_row()
 # DO NOT CHANGE!!!
@@ -34,12 +43,16 @@ def process_file():
 # process_row should splits row into the different csv table files
 # CHANGE!!!
 def process_row(row):
-    outwriter.writerow(row)
+    outwriter_a.writerow([row[0],row[1],row[2],row[4],row[5]])
+    outwriter_o.writerow([row[9],row[10],row[11]])
+    outwriter_e.writerow([row[13],row[12],row[10],row[9]])
+    outwriter_t.writerow([row[6],row[7]])
+    outwriter_c.writerow([row[0],row[13],row[9],row[10],row[14],row[3]])
 
 # return the list of all tables
 # CHANGE!!!
 def get_names():
-    return ["olympics"]
+    return ["Athlete",'Olympics','Event','Team','Competed at']
 
 
 if __name__ == "__main__":
